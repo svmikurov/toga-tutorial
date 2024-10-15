d0767fcfb1683d8d043b7d9f42d86e44240dec2b
Sergei Mikurov <454004@mail.ru>
2024-10-15 22:57:03 +0500

    Add exemples

---
+++ README.rst
@@ -0,0 +1,17 @@
+=============
+Toga tutorial
+=============
+
+`Toga documentation latest (dev) <https://toga.readthedocs.io/en/latest/index.html>`_
+
+To run **helloworld.py**:
+
+    $ python -m helloworld
+
+To run **temperature_converter.py**:
+
+    $ python -m temperature_converter
+
+To run **first_app.py**:
+
+    $ python -m first_app
--- helloworld.py
+++ helloworld.py
@@ -1,24 +1,43 @@
-import toga
+import  toga
+from toga.style import Pack
+
+btn_style = Pack(
+    padding=0,
+    flex=1,
+)
 
 
-def button_hello(widget):
-    print("Привет!")
+def button_handler(widget):
+    print('hello')
 
 
 def build(app):
     box = toga.Box()
 
-    button = toga.Button("Hello world", on_press=button_hello)
+    btn_1 = toga.Button('1', style=btn_style, on_press=button_handler)
+    btn_1.style.flex = 1
+    btn_2 = toga.Button('2', style=btn_style, on_press=button_handler)
+    btn_2.style.flex = 1
+    btn_3 = toga.Button('3', style=btn_style, on_press=button_handler)
+    btn_3.style.flex = 2
+
+    button = toga.Button('Hello world', on_press=button_handler)
     button.style.padding = 50
     button.style.flex = 1
-    box.add(button)
+
+    # box.add(button)
+    box.add(btn_1, btn_2, btn_3)
 
     return box
 
 
 def main():
-    return toga.App("First App", "org.beeware.toga.tutorial", startup=build)
+    return toga.App(
+        "First App",
+        "org.beeware.toga.tutorial",
+        startup=build
+    )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main().main_loop()
